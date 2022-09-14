0376b5e3280f0d0f2f996ecb5ba6c6513a00c778
Maxim Letkov <letkovmaxim@gmail.com>
2022-09-14 17:59:50 +0300

    Реализован базовый каркас Spring Boot приложения со стартовой страницей

---
+++ src/main/java/org/sbtitcourses/mdwiki/MdWikiApplication.java
@@ -0,0 +1,13 @@
+package org.sbtitcourses.mdwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sbtitcourses/mdwiki/MdWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sbtitcourses.mdwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
