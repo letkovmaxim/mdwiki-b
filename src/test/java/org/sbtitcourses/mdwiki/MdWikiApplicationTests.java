505d48a057a95df07a91160df18d565c5d317c4d
Maxim Letkov <letkovmaxim@gmail.com>
2022-09-15 20:58:37 +0300

    Убран пустой тест

--- src/test/java/org/sbtitcourses/mdwiki/MdWikiApplicationTests.java
+++ src/test/java/org/sbtitcourses/mdwiki/MdWikiApplicationTests.java
@@ -1,13 +1,7 @@
 package org.sbtitcourses.mdwiki;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MdWikiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
