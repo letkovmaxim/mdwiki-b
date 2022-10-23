a58663dc1d27f27300b29d9eab34ccea8a18b13a
Maxim Letkov <letkovmaxim@gmail.com>
2022-10-23 22:51:00 +0300

    Добавлены недостающие комментарии

--- backend/src/main/java/org/sbtitcourses/mdwiki/service/PageCrudService.java
+++ backend/src/main/java/org/sbtitcourses/mdwiki/service/PageCrudService.java
@@ -9,9 +9,43 @@ import java.util.List;
  * Интерфейс сервиса CRUD операций над сущностью Page
  */
 public interface PageCrudService {
+
+    /**
+     * Создание новой страницы
+     * @param pageToCreate страница, которую нужно сохранить
+     * @return созданную страницу
+     */
     Page create(Page pageToCreate);
+
+    /**
+     * Получение всех страниц в пространстве
+     * @param space пространство, в котором нужно получить страницы
+     * @param bunch номер группы, на которые разбиваются страницы
+     * @param size количество страниц в группе
+     * @return список найденых страниц
+     */
     List<Page> getAll(Space space, int bunch, int size);
+
+    /**
+     * Получение страницы в пространстве по её ID
+     * @param id ID страницы
+     * @param space пространство, в котором нужно найти страницу
+     * @return найденую страницу
+     */
     Page get(int id, Space space);
+
+    /**
+     * Обновление странцы в пространстве по её ID
+     * @param id ID страницы
+     * @param space пространство, в котором нужно обновить страницу
+     * @param pageToUpdateWith объект Page, значениями полей которого нужно обновить страницу
+     * @return обновленную страницу
+     */
     Page update(int id, Space space, Page pageToUpdateWith);
+
+    /**
+     * Удаление страницы по её ID
+     * @param id ID страницы
+     */
     void delete(int id);
 }
--- backend/src/main/java/org/sbtitcourses/mdwiki/service/SpaceCrudService.java
+++ backend/src/main/java/org/sbtitcourses/mdwiki/service/SpaceCrudService.java
@@ -8,9 +8,38 @@ import java.util.List;
  * Интерфейс сервиса CRUD операций над сущностью Space
  */
 public interface SpaceCrudService {
+
+    /**
+     * Создание нового пространства
+     * @param spaceToCreate пространство, которое нужно сохранить
+     * @return созданное пространство
+     */
     Space create(Space spaceToCreate);
+
+    /**
+     * Получение всех пространств
+     * @return список всех пространств
+     */
     List<Space> getAll();
+
+    /**
+     * Получение пространства по его ID
+     * @param id ID пространства
+     * @return найденное пространство
+     */
     Space get(int id);
+
+    /**
+     * Обновление пространства по его ID
+     * @param id ID пространства
+     * @param spaceToUpdateWith объект класса Space, значениями полей которого нужно обновить пространство
+     * @return обновленное пространство
+     */
     Space update(int id, Space spaceToUpdateWith);
+
+    /**
+     * Удаление пространства по его ID
+     * @param id ID пространства
+     */
     void delete(int id);
 }
